0f200bf73337c7f812b0d2b3c46e2bfbf0456954
git <client@funxdata.com>
2025-07-27 23:18:27 +0800

    update js update

--- script/public.ts
+++ script/public.ts
@@ -1,5 +1,6 @@
 import { resolve, dirname, fromFileUrl } from "@std/path";
 import { uploadFileToUpyun, readFilesRecursive } from "./upfile.ts";
+import dayjs from "npm:dayjs";
 
 // 获取当前模块的目录路径
 const __dirname = dirname(fromFileUrl(import.meta.url));
@@ -16,3 +17,7 @@ const dir_files = await readFilesRecursive(publicDir);
 for (const file of dir_files) {
   await uploadFileToUpyun("uidocs", file.fullPath, file.relativePath);
 }
+
+// 上传stylesheet js 文件
+  const style_version = dayjs().format("YY_MM_DD")
+  await uploadFileToUpyun("assets", "../assets/app.js", "app"+style_version+".js");
